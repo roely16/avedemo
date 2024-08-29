add67d720b9ce9f2c1a3a675c43317303118f2af
Herson Chur <hchur@eaglehubs.com>
2024-08-28 20:13:29 -0600

    added protocol card

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,9 +26,9 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'My Account',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'person-circle' : 'person-circle-outline'} color={color} />
           ),
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,94 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet, View, ScrollView, SafeAreaView, FlatList } from 'react-native';
+import { Avatar, Button, Card, Text } from 'react-native-paper';
 
 export default function HomeScreen() {
+
+  const Hello = () => {
+    return (
+      <Text variant="titleMedium">Hello <Text style={{ fontWeight: 'bold' }} variant="titleMedium">Jane</Text></Text>
+    )
+  }
+
+  const Title = () => {
+    return (
+      <View style={styles.titleContainer}>
+        <Text style={{ fontWeight: 'bold' }} variant="displaySmall">{"Manage Your\nProtocols"}</Text>
+      </View>
+    )
+  }
+
+  const ProtocolSection = () => {
+
+    const protocols = [
+      {
+        id: 1,
+        name: 'Emergency Alarm',
+        tasks: 3,
+        participants: [
+          {
+            avatarUrl: 'https://xsgames.co/randomusers/avatar.php?g=male'
+          },
+          {
+            avatarUrl: 'https://xsgames.co/randomusers/avatar.php?g=female'
+          },
+          {
+            avatarUrl: 'https://xsgames.co/randomusers/avatar.php?g=pixel'
+          }
+        ]
+      },
+      {
+        id: 2,
+        name: 'Fire Alarm',
+        tasks: 3
+      },
+      {
+        id: 3,
+        name: 'AI Assistant App Design',
+        tasks: 3
+      }
+    ];
+
+    const ProtocolCard = ({ tasks, name, participants = [] }: { tasks: number; name: string; participants: Array } ) => {
+      return (
+        <Card style={{ width: '48%', marginBottom: 20 }} mode="contained">
+          <Card.Content>
+            <Text variant="bodyMedium">{`${tasks} task`}</Text>
+            <Text variant="titleLarge">{name}</Text>
+            <View style={{ marginTop: 10, flexDirection: 'row' }}>
+              {
+                participants.map((participant, index) => (
+                  <Avatar.Image source={{ uri: participant.avatarUrl }} style={{ marginRight: -5, zIndex: index }} key={index} size={32} />
+                ))
+              }
+            </View>
+          </Card.Content>
+        </Card>
+      )
+    };
+
+    return (
+      <View style={{ marginTop: 30 }}>
+        <View style={{ marginBottom: 20, justifyContent: 'space-between', flexDirection: 'row' }}>
+          <Text style={{ fontWeight: 'bold' }} variant="titleLarge">Protocols</Text>
+          <Button icon="plus" style={{ borderRadius: 10 }} mode="text">Add</Button>
+        </View>
+        <FlatList columnWrapperStyle={{ justifyContent: 'space-between' }} numColumns={2} data={protocols} renderItem={({item}) => <ProtocolCard {...item} />}></FlatList>
+      </View>
+    )
+  };
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+      <ScrollView style={{ padding: 20 }}>
+        <Hello />
+        <Title />
+        <ProtocolSection />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    marginTop: 20
+  }
 });
