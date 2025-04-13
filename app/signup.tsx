7777be5eb530a62f234c358b25dad36006a73aac
Herson Chur <hchur@eaglehubs.com>
2025-04-12 19:04:12 -0600

    feat: add sign up form

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,80 +1,65 @@
 import { View, SafeAreaView, TouchableOpacity, StyleSheet } from 'react-native';
-import { Text, Button, Icon, TextInput } from 'react-native-paper';
+import { Text, Icon } from 'react-native-paper';
 import { Link } from 'expo-router';
-import { getAuth } from '@react-native-firebase/auth';
-import { useState } from 'react';
+import { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import { Form } from './signup/components/Form';
+import { useSignUp } from './signup/hooks/useSignUp';
 
+const Header = () => {
+  return (
+    <View>
+      <Link href="/" asChild>
+        <TouchableOpacity style={styles.goBackButton}>
+          <Icon source="arrow-left" size={30} />
+        </TouchableOpacity>
+      </Link>
+      <Text variant="displaySmall">Sign Up Here</Text>
+      <Text variant="headlineSmall">Create an account</Text>
+    </View>
+  );
+};
+
+export interface SignUpResponse {
+  success: boolean;
+  user?: FirebaseAuthTypes.User;
+  error?: string;
+}
 
 export default function SignUp() {
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
-  const Header = () => {
-    return (
-      <View>
-        <Link href="/" asChild>
-          <TouchableOpacity style={styles.goBackButton}>
-            <Icon source='arrow-left' size={30} />
-          </TouchableOpacity>
-        </Link>
-        <Text variant='displaySmall'>Sign Up Here</Text>
-        <Text variant="headlineSmall">Create an account</Text>
-      </View>
-    )
-  }
-
-  const handleSignUp = async () => {
-    try {
-      const auth = getAuth();
-      const userCredential = await auth.createUserWithEmailAndPassword(email, password);
-      return {
-        success: true,
-        user: userCredential.user
-      };
-    } catch (error: any) {
-      console.log(error);
-      return {
-        success: false,
-        error: error.message
-      };
-    }
-  };
-
-  const Form = (): React.ReactNode => {
-    return (
-      <View style={styles.formContainer}>
-        <View style={styles.formInputContainer}>
-          <Text variant='titleMedium'>Name</Text>
-          <TextInput dense mode="outlined" />
-        </View>
-        <View style={styles.formInputContainer}>
-          <Text variant='titleMedium'>Email</Text>
-          <TextInput onChangeText={setEmail} keyboardType="email-address" dense mode="outlined" />
-        </View>
-        <View style={styles.formInputContainer}>
-          <Text variant='titleMedium'>Password</Text>
-          <TextInput secureTextEntry dense mode="outlined" />
-        </View>
-        <View>
-          <Text variant='titleMedium'>Confirm Password</Text>
-          <TextInput secureTextEntry dense mode="outlined" />
-        </View>
-        <Button onPress={handleSignUp} style={styles.confirmButton} mode="contained">Register</Button>
-      </View>
-    )
-  }
+  const {
+    name,
+    setName,
+    email,
+    password,
+    confirmPassword,
+    setEmail,
+    setPassword,
+    setConfirmPassword,
+    handleSignUp,
+    isLoading
+  } = useSignUp();
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.contentContainer}>
         <Header />
-        <Form></Form>
+        <Form
+          name={name}
+          setName={setName}
+          email={email}
+          password={password}
+          confirmPassword={confirmPassword}
+          setEmail={setEmail}
+          setPassword={setPassword}
+          setConfirmPassword={setConfirmPassword}
+          onSubmit={handleSignUp}
+          isLoading={isLoading}
+        />
       </View>
     </SafeAreaView>
-  )
-};
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
---
+++ app/signup/components/Form.styles.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  contentContainer: {
+    paddingHorizontal: 10,
+    marginTop: 30
+  },
+  goBackButton: {
+    marginBottom: 10
+  },
+  formContainer: {
+    marginTop: 30
+  },
+  formInputContainer: {
+    marginBottom: 20
+  },
+  confirmButton: {
+    marginTop: 40,
+    borderRadius: 10
+  },
+  passwordRule: {
+    color: 'black',
+    fontSize: 12,
+    marginLeft: 4,
+  },
+  passwordRuleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 4
+  }
+});
